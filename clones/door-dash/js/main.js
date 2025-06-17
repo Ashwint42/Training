6d6a9d5e0d8babd2010a044f567c6bd9d55977c0
Ashwin T <ashwint0427@gmail.com>
2025-06-17 04:08:43 +0000

    Added franchise section button toggle

--- clones/door-dash/js/main.js
+++ clones/door-dash/js/main.js
@@ -7,6 +7,11 @@ const topChains = [
 
 const topCities = [
     "New York", "Houston", "San Francisco", "Vancouver, BC", "Bend", "Salt Lake City", "Birmingham", "Victoria", "Christchurch", "Huntsville",
+    "Los Angeles", "Brooklyn", "Seattle", "Miami", "Kamloops"
+];
+
+const topCitiesRemaining = [
+    "New York", "Houston", "San Francisco", "Vancouver, BC", "Bend", "Salt Lake City", "Birmingham", "Victoria", "Christchurch", "Huntsville",
     "Los Angeles", "Brooklyn", "Seattle", "Miami", "Kamloops", "Jacksonville", "New Orleans", "Columbia", "Milwaukee", "Des Moines",
     "Toronto", "San Diego", "Atlanta", "San Antonio", "Tulsa", "Colorado Springs", "Lexington", "Scottsdale", "Pittsburgh", "Quebec City",
     "Chicago", "Las Vegas", "Queens", "Tallahassee", "Austin", "Wilmington", "Orlando", "Augusta", "Ft. Worth", "All Cities"
@@ -19,10 +24,16 @@ const topCuisines = [
 
 
 
-
 const franchiseLinks = document.querySelectorAll('#franchise-links');
 const franchiseData = document.querySelector('.franchise-data');
-const defaultLink = document.querySelector('.default-link')
+const defaultLink = document.querySelector('.default-link');
+
+let seeMoreButton = document.querySelector('.see-more-button');
+let seeLessButton = document.querySelector('.see-less-button');
+let seeMoreContainer = document.querySelector('.see-more-container ');
+let seeLessContainer = document.querySelector('.see-less-container ');
+
+let renderedList;
 
 franchiseLinks.forEach(link => {
 
@@ -36,21 +47,22 @@ franchiseLinks.forEach(link => {
         e.target.classList.add('clicked-style');
         e.target.style.color = 'black'
 
-        let renderedList;
-
         if (e.target.innerText === "Top Cuisines") {
             renderedList = '';
             topCuisines.forEach(data => {
                 renderedList += `<li>${data}</li>`
             });
+            seeLessContainer.style.display = "none";
+            seeMoreContainer.style.display = "none";
         }
 
         else if (e.target.innerText === "Top Cities") {
+
             renderedList = '';
             topCities.forEach(data => {
                 renderedList += `<li>${data}</li>`
             });
-
+            seeMoreContainer.style.display = "flex";
         }
 
         else {
@@ -58,15 +70,39 @@ franchiseLinks.forEach(link => {
             topChains.forEach(data => {
                 renderedList += `<li>${data}</li>`
             });
+            seeLessContainer.style.display = "none";
+            seeMoreContainer.style.display = "none";
         }
 
         franchiseData.innerHTML = renderedList
     })
 })
 
-window.addEventListener('load', () => {
-    defaultLink.click();
+
+seeMoreButton.addEventListener('click', () => {
+    console.log('cl')
+    renderedList = '';
+    topCitiesRemaining.forEach(data => {
+        renderedList += `<li>${data}</li>`
+    });
+    franchiseData.innerHTML = renderedList;
+
+    seeLessContainer.style.display = "flex";
+    seeMoreContainer.style.display = "none";
+})
+
+seeLessButton.addEventListener('click', () => {
+    renderedList = '';
+    topCities.forEach(data => {
+        renderedList += `<li>${data}</li>`
+    });
+    franchiseData.innerHTML = renderedList;
+
+    seeLessContainer.style.display = "none";
+    seeMoreContainer.style.display = "flex";
 })
 
 
-
+window.addEventListener('load', () => {
+    defaultLink.click();
+})
